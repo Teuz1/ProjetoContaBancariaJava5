8082ce79aafc194514cda4103d6aa323c0431a25
Teuz1 <matheussouzak18@gmail.com>
2023-11-29 00:16:24 -0300

    conta.controller

---
+++ Controllerr.java
@@ -0,0 +1,46 @@
+package Controller;
+
+public class Controllerr {
+
+	public static void main(String[] args) {
+		
+		int dia_semana = 0;
+		switch (dia_semana)
+		{
+		case 1:
+		System.out.println("Domingo");
+		break;
+		case 2:
+		System.out.println("Segunda-Feira");
+		break;
+		case 3:
+		System.out.println("Terça-Feira");
+		break;
+		case 4:
+		System.out.println("Quarta-Feira");
+		break;
+		case 5:
+		System.out.println("Quinta-Feira");
+		break;
+		case 6:
+		System.out.println("Sexta-Feira");
+		break;
+		case 7:
+		System.out.println("Sábado");
+		break;
+		}
+		switch (dia_semana)
+		{
+		case 1 -> System.out.println("Domingo");
+		case 2 -> System.out.println("Segunda-Feira");
+		case 3 -> System.out.println("Terça-Feira");
+		case 4 -> System.out.println("Quarta-Feira");
+		case 5 -> System.out.println("Quinta-Feira");
+		case 6 -> System.out.println("Sexta-Feira");
+		case 7 -> System.out.println("Sábado");
+		}
+	
+	
+	}
+
+}
